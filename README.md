3fd97ba9695b7a3c4f2a1872c7c7bd6b8c45538b
vaulverin <vauulin@gmail.com>
2017-05-01 23:30:38 +0500

    https only

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # xsollatest
-xsolla test
+
 *Installation:*
 1. download vagrant box with `vagrant box add phalconphp/xenial64`
-2. add `192.168.50.4 xsollatest.dev` to yout hosts file
+2. add `192.168.50.4 xsollatest.dev` to your hosts file
 3. run `vagrant up`
--- workspace/xsollatest/public/index.php
+++ workspace/xsollatest/public/index.php
@@ -1,4 +1,15 @@
 <?php
+if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
+    if(!headers_sent()) {
+        header("Status: 301 Moved Permanently");
+        header(sprintf(
+            'Location: https://%s%s',
+            $_SERVER['HTTP_HOST'],
+            $_SERVER['REQUEST_URI']
+        ));
+        exit();
+    }
+}
 use Phalcon\Di\FactoryDefault;
 
 error_reporting(E_ALL);
