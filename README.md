2b39faece213d83c564f9448761319116c154510
vaulverin <vauulin@gmail.com>
2017-05-03 10:26:21 +0000

    migrations added, model added

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 1. download vagrant box with `vagrant box add phalconphp/xenial64`
 2. add `192.168.50.4 xsollatest.dev` to your hosts file
 3. run `vagrant up`
+4. execute command `mysql < app/migrations/db_create.sql` from project root
--- workspace/xsollatest/app/app.php
+++ workspace/xsollatest/app/app.php
@@ -11,32 +11,33 @@ $app->get('/', function () {
     echo $this['view']->render('index');
 });
 
+// Create new user
 $app->post('/add-user', function() use ($app) {
     $user = $app->request->getJsonRawBody();
 
 });
 
+// Get file list for current user
 $app->get('/list', function() use ($app) {
 
 });
 
+// File name can contains only letters and digits
 $filename = '{file:[A-z0-9]+.[A-z]{1,5}';
-$app->get('/list', function() use ($app) {
 
-});
-
+// Get file with <filename>
 $app->get('/'.$filename, function(File $file) use ($app) {
 
 });
-
+// Create file with <filename>
 $app->post('/'.$filename, function(File $file) use ($app) {
 
 });
-
+// Update file with <filename>
 $app->update('/'.$filename, function(File $file) use ($app) {
 
 });
-
+// Get file metadata with <filename>
 $app->get('/'.$filename.'/meta', function(File $file) use ($app) {
 
 });
